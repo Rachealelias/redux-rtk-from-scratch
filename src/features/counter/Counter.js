2c3a16791d4c856661bfedde1327b4dadaf37490
Racheal Amuneke <rachealsamuel708@gmail.com>
2022-05-26 19:25:17 -0400

    class 1 ended

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,19 +1,24 @@
 import React, {useState} from 'react'
+import { increment, decrement, reset, incrementByAmount } from './CounterSlice'
+import {useSelector, useDispatch} from "react-redux"
 
 const Counter = () => {
-    const [count, setCount] = useState(0)
+    //const [count, setCount] = useState(0)
+    const count = useSelector(state => state.counter.value)
+    const dispatch = useDispatch()
 
-    const increment = () => setCount(currentCount => currentCount + 1)
-    const decrement = () => setCount(currentCount => currentCount - 1)
-    const incrementBy = (amount) => setCount(currentCount => currentCount + amount)
+    // const increment = () => setCount(currentCount => currentCount + 1)
+    // const decrement = () => setCount(currentCount => currentCount - 1)
+    // const incrementBy = (amount) => setCount(currentCount => currentCount + amount)
 
     return (
         <div>
             <h2>Counter</h2>
             <p>Current Count: {count}</p>
-            <button onClick={increment}>Increment</button>
-            <button onClick={decrement}>Decrement</button>
-            <button onClick={() => incrementBy(5)}>Increment By 5</button>
+            <button onClick={() => dispatch(reset())}>Reset</button>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <button onClick={() => dispatch(incrementByAmount(5))}>Increment By 5</button>
         </div>
     )
 }
